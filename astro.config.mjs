18c197e2565dbee08a1175292e6a8dde1cd7a241
Eric Minassian <minassianeric2848@gmail.com>
2024-08-08 21:54:46 -0700

    use astro for ssg

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://www.ericminassian.com",
+  integrations: [tailwind(), sitemap()],
+});
