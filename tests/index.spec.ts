a4fafbe956f5db48975647f3092a6de71307b214
Eric Minassian <minassianeric2848@gmail.com>
2024-12-22 23:19:33 -0800

    feat: config file for all personal information

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,47 +1,35 @@
 import { expect, test } from "@playwright/test";
+import { personalConfig, siteConfig } from "../src/config";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("/");
 });
 
 test("meta is correct", async ({ page }) => {
-  await expect(page).toHaveTitle("Eric Minassian");
+  await expect(page).toHaveTitle(siteConfig.title);
 });
 
 test("main heading is correct", async ({ page }) => {
   const mainHeadingSelector = "h1";
   await expect(page.locator(mainHeadingSelector)).toHaveText(
-    "hey, i'm eric 👋"
+    `hey, i'm ${personalConfig.name} ${personalConfig.emoji}`
   );
 });
 
 test("should have correct external links", async ({ page }) => {
-  const links = [
-    { href: "https://linkedin.com/in/minassian-eric", text: "linkedin" },
-    { href: "/Eric_Minassian_resume.pdf", text: "resume" },
-    { href: "https://github.com/eric-minassian", text: "github" },
-    { href: "mailto:eric@ericminassian.com", text: "email" },
-  ];
-
-  for (const link of links) {
+  for (const link of personalConfig.externalLinks) {
     const externalLink = page.locator(`a[href="${link.href}"]`);
     await expect(externalLink).toHaveText(link.text);
   }
 });
 
 test("should render Badge components correctly", async ({ page }) => {
-  const badges = [
-    { href: "https://amazon.com", text: "Amazon" },
-    {
-      href: "https://www.aboutamazon.com/what-we-do/devices-services/project-kuiper",
-      text: "Project Kuiper",
-    },
-    { href: "https://tech.walmart.com", text: "Walmart Global Tech" },
-    { href: "https://uci.edu", text: "University of California, Irvine" },
-  ];
+  const regex = /\[([^\]]+)\]\(([^)]+)\)/g;
+  const matches = personalConfig.description.matchAll(regex);
 
-  for (const badge of badges) {
-    const badgeElement = page.locator(`a[href="${badge.href}"] span.no-prose`);
-    await expect(badgeElement).toHaveText(badge.text);
+  for (const match of matches) {
+    const [_, text, href] = match;
+    const badgeElement = page.locator(`a[href="${href}"] span.no-prose`);
+    await expect(badgeElement).toHaveText(text);
   }
 });
