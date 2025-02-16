5c9b85650b51b8fb875556632963ce2c2b574b88
Eric Minassian <minassianeric2848@gmail.com>
2025-02-15 18:30:48 -0800

    fix: disable github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,20 @@ import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.ericminassian.com",
   integrations: [sitemap()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
   vite: {
     plugins: [tailwindcss()],
   },
-  devToolbar: { enabled: false },
   prefetch: true,
+  devToolbar: { enabled: false },
 });
