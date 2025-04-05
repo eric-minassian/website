aea98e2c47cee7e0da9096e61425a439ef6f86b5
Eric Minassian <minassianeric2848@gmail.com>
2025-04-04 17:54:06 -0700

    feat: github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,12 @@ import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.ericminassian.com",
   integrations: [sitemap()],
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
   vite: {
     plugins: [tailwindcss()],
   },
   prefetch: true,
-  devToolbar: { enabled: false },
 });
